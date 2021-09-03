bda518e50d15eb577649017e7f20a6368758eb51
StevenKamwaza <bed-com-10-17@cc.ac.mw>
2021-09-03 11:47:56 +0200

    vo

--- app/src/main/java/com/chesteve/last/useermap/MapsActivity.java
+++ app/src/main/java/com/chesteve/last/useermap/MapsActivity.java
@@ -5,7 +5,8 @@ import android.os.Bundle;
 import androidx.fragment.app.FragmentActivity;
 
 import com.chesteve.last.R;
-import com.chesteve.last.useermap.databinding.ActivityMaps2Binding;
+import com.chesteve.last.databinding.ActivityMapsBinding;
+import com.chesteve.last.useermap.databinding.ActivityMapsBinding;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -16,13 +17,13 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    private ActivityMaps2Binding binding;
+    private ActivityMapsBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMaps2Binding.inflate(getLayoutInflater());
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
