025a559b5c640da5c8b5e01e8a712062b0cbf94d
StevenKamwaza <bed-com-10-17@cc.ac.mw>
2021-09-06 00:19:31 +0200

    db

---
+++ app/src/test/java/com/chesteve/last/SighUpActivityTest.java
@@ -0,0 +1,15 @@
+package com.chesteve.last;
+
+import junit.framework.TestCase;
+
+public class SighUpActivityTest extends TestCase {
+
+    public void testOnCreate() {
+    }
+
+    public void testBackLoginNow() {
+    }
+
+    public void testRegisterNewUser() {
+    }
+}
